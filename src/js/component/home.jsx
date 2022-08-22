019fb37c3d80572119c51c55d54c2f4aa15382b9
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-08-22 02:19:20 -0400

    fixed main functionality and render issues

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,44 +1,41 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect , useRef} from "react";
 import Task from "./Task.jsx";
 
 //create your first component
 const Home = () => {
-	const[tasks,setTasks] = useState([])
-	const[text,setText] = useState(null)
-	const[render,setRender] = useState(false)
-	useEffect(() => {
-		const keyDownHandler = (e) => {
-		  if (e.key === 'Enter') {
-			e.preventDefault();
-			document.getElementById("inputTask").value = '';
+	const [tasks,setTasks] = useState([])
+	const taskNumber = useRef(0)
+	const [text,setText] = useState("")
+
+	const handleKeyDown = (e) => {
+		if(e.key === "Enter" && text !== ""){
+			console.log("submitted")
+			console.log(text)
 			var new_tasks = tasks
 			new_tasks.push(text)
-			setTasks(new_tasks)
 			setText("")
-		  }
-		};
-	
-		document.addEventListener('keydown', keyDownHandler);
-		setRender(true)
-		return () => {
-		  document.removeEventListener('keydown', keyDownHandler);
-		};
-	  }, [text]);
-
-	
-	const handleClick = (index) => {
-		var new_tasks = tasks;
-		new_tasks.splice(index,1)
-		setTasks(new_tasks)
+			setTasks(new_tasks)
+			taskNumber.current = taskNumber.current + 1;
+		}else if (e.key === "Enter"){
+			console.log("Error! Add a task first!")
+		}
 	}
-	
+
+	const handleClick = (index) => {
+		tasks.splice(index,1);
+		setTasks([...tasks])
+		taskNumber.current = taskNumber.current - 1
+	}
+
+
 	return (
 		<div className="body">
 			<div className="paper">
 				<div className="inputBox">
-					<input id="inputTask" type="text" onChange={(e) => setText(e.target.value)} placeholder="Add a task here..."></input>
+					<input id="inputTask" type="text" value={text} onKeyDown={(e) => handleKeyDown(e)} onChange={(e) => {setText(e.target.value)}} placeholder="Add a task here..."></input>
 				</div>
-				{tasks.map((item,index) => <Task text={item} key={index} onClick={() => {handleClick(index)}}/>)}
+				{(tasks.length !== 0) ? tasks.map((item,index) => <Task text={item} key={index} onClick={() => {handleClick(index)}}/>) : <div className="emptyTask">No tasks!</div>}
+				{(tasks.length !== 0) ? <div className="footer">{taskNumber.current} tasks left...</div> : null}
 			</div>
 		</div>
 	);
